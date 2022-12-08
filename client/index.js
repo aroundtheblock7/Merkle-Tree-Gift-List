5d2721305f511959e18a408e1cf7aedcb994d7ef
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2022-12-08 06:29:30 -0500

    update

---
+++ client/index.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+
+async function main() {
+
+  const merk = new MerkleTree(niceList);
+  const name = "Diana Hand";
+  const index = niceList.findIndex(n => n === name);
+  const proof = merk.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    "name": name,
+    "proof": proof
+  });
+
+  console.log({ gift });
+}
+
+main();
